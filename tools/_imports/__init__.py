5a09d0fc75fee59e5d9a040101c563c8dc52c8c8
aocn <willion@sohu.com>
2019-05-13 14:28:07 +0800

    change some conf

---
+++ tools/_imports/__init__.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+
+
+# request
+import urllib
+from urllib import request
+
+
+# basic tools
+import random
+import chardet
+
+
+# get xpath
+import lxml
+from lxml import etree
+
+
+# get html ele tools
+from pyquery import PyQuery as pq
+
+
+# 解析json 
+import demjson
+
+
+# regex-lib
+import re
+
+
+# get html ele tools
+from pyquery import PyQuery as pq
+
+
+
+
+
